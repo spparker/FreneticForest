72d0c763b0a41d87621407ad23215e78678238bb
spparker <sean.p.parker@gmail.com>
2023-02-01 17:34:36 -0800

    Mouse Wheel Zoom

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -13,7 +13,12 @@ public class CameraControl : MonoBehaviour
     }
 
     LinkedList<CardinalDir> directionCycle = new LinkedList<CardinalDir>();
-    LinkedListNode<CardinalDir> currentDir;
+    LinkedListNode<CardinalDir> currentDir; // Direction the camera is placed (eg. SOUTH would be looking north)
+
+    private Vector3 _objToCamNorth;
+    private Vector3 _objToCamSouth;
+    private Vector3 _objToCamEast;
+    private Vector3 _objToCamWest;
 
 
     public float IsoBuffer => ForestSetup.Instance.ArenaSize * 0.6f;
@@ -21,6 +26,13 @@ public class CameraControl : MonoBehaviour
 
     Transform mainCamTransform;
     Transform miniMapCamTransform;
+
+
+    public const float ZOOM_RATE = 0.05f;
+    public const float MIN_ZOOM_DIST = 0.1f;
+    //private float _maxZoomDistance; // Find from initial setup
+    private Vector3 _curLookPos; // Current spot on map that is targeted
+    private float _curZoom = 1;
 
     void Awake()
     {
@@ -31,21 +43,76 @@ public class CameraControl : MonoBehaviour
     void Start()
     {
         mainCamTransform = Camera.main.transform;
+        _curLookPos = new Vector3(ForestSetup.Instance.HomeTree.transform.position.x, 
+                                    ForestSetup.Instance.HomeTree.transform.position.y,
+                                    ForestSetup.Instance.HomeTree.transform.position.z);
+
         currentDir = directionCycle.First; // Start at South
         SetupMainCamera( ForestSetup.Instance.ForestSettings.forestScale * 5f);
         SetupMinimapCamera();
+
+        //_maxZoomDistance = Vector3.Magnitude(_curLookPos - mainCamTransform.position);
     }
 
     void Update()
     {
+        // Map Zoom
+        CheckForZoom();
+
+        // Map Rotation
         if(Input.GetKeyDown(KeyCode.Q))
             RotateLeft();
 
         if(Input.GetKeyDown(KeyCode.E))
             RotateRight();
+
+        // Map Panning
+        /*if(Input.GetKeyDown(KeyCode.W))
+            PanForward();
+
+        if(Input.GetKeyDown(KeyCode.S))
+            PanBackwards();
+
+        if(Input.GetKeyDown(KeyCode.A))
+            PanLeft();
+
+        if(Input.GetKeyDown(KeyCode.D))
+            PanRight();*/
+
+        UpdateCameraPosition(currentDir.Value);
     }
 
-    public void RotateLeft()
+    private void UpdateCameraPosition(CardinalDir dir)
+    {
+        if(dir == CardinalDir.NORTH && _objToCamNorth != null)
+        {
+            mainCamTransform.position = _curLookPos + _objToCamNorth * _curZoom;
+            mainCamTransform.LookAt(_curLookPos);
+        }
+        else if(dir == CardinalDir.SOUTH && _objToCamSouth != null)
+        {
+            mainCamTransform.position = _curLookPos + _objToCamSouth * _curZoom;
+            mainCamTransform.LookAt(_curLookPos);
+        }
+        else if(dir == CardinalDir.EAST && _objToCamEast != null)
+        {
+            mainCamTransform.position = _curLookPos + _objToCamEast * _curZoom;
+            mainCamTransform.LookAt(_curLookPos);
+        }
+        else if(dir == CardinalDir.WEST && _objToCamWest != null)
+        {
+            mainCamTransform.position = _curLookPos + _objToCamWest * _curZoom;
+            mainCamTransform.LookAt(_curLookPos);
+        }
+    }
+
+    private void CheckForZoom()
+    {
+        _curZoom = Mathf.Clamp(_curZoom - Input.mouseScrollDelta.y * ZOOM_RATE
+                                , MIN_ZOOM_DIST, 1.0f);
+    }
+
+    private void RotateLeft()
     {
         miniMapCamTransform.Rotate(Vector3.forward, 90);
         if(currentDir.Next == null)
@@ -53,9 +120,10 @@ public class CameraControl : MonoBehaviour
         else
             currentDir = currentDir.Next;
         PositionMainCameraCardinalDirection(currentDir.Value);
+        //_curZoom = 1.0f;
     }
 
-    public void RotateRight()
+    private void RotateRight()
     {
         miniMapCamTransform.Rotate(Vector3.forward, -90);
         if(currentDir.Previous == null)
@@ -63,6 +131,7 @@ public class CameraControl : MonoBehaviour
         else
             currentDir = currentDir.Previous;
         PositionMainCameraCardinalDirection(currentDir.Value);
+        //_curZoom = 1.0f;
     }
 
     private void SetupMainCamera(float max_pos)
@@ -86,24 +155,32 @@ public class CameraControl : MonoBehaviour
             mainCamTransform.position = new Vector3(0, IsoCamHeight,
                                     ForestSetup.Instance.ArenaSize + IsoBuffer);
             mainCamTransform.LookAt(ForestSetup.Instance.HomeTree.transform);
+            if(_objToCamNorth == Vector3.zero)
+                _objToCamNorth = ForestSetup.Instance.HomeTree.transform.position + mainCamTransform.position;
         }
         else if(dir == CardinalDir.SOUTH)
         {
             mainCamTransform.position = new Vector3(0, IsoCamHeight,
                                     -ForestSetup.Instance.ArenaSize - IsoBuffer);
             mainCamTransform.LookAt(ForestSetup.Instance.HomeTree.transform);
+            if(_objToCamSouth == Vector3.zero)
+                _objToCamSouth = ForestSetup.Instance.HomeTree.transform.position + mainCamTransform.position;
         }
         else if(dir == CardinalDir.EAST)
         {
             mainCamTransform.position = new Vector3(ForestSetup.Instance.ArenaSize + IsoBuffer,
                                                 IsoCamHeight, 0);
             mainCamTransform.LookAt(ForestSetup.Instance.HomeTree.transform);
+            if(_objToCamEast == Vector3.zero)
+                _objToCamEast = ForestSetup.Instance.HomeTree.transform.position + mainCamTransform.position;
         }
         else if(dir == CardinalDir.WEST)
         {
             mainCamTransform.position = new Vector3(-ForestSetup.Instance.ArenaSize - IsoBuffer,
                                                 IsoCamHeight, 0);
             mainCamTransform.LookAt(ForestSetup.Instance.HomeTree.transform);
+            if(_objToCamWest == Vector3.zero)
+                _objToCamWest = ForestSetup.Instance.HomeTree.transform.position + mainCamTransform.position;
         }
     }
 

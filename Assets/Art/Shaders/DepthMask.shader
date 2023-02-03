ff91cc894e4a3aa5dc6d798f17b20155aba0cc2c
spparker <sean.p.parker@gmail.com>
2023-02-03 12:11:02 -0800

    Very good hole

---
+++ Assets/Art/Shaders/DepthMask.shader
@@ -0,0 +1,11 @@
+Shader "Custom/DepthMask" {
+    SubShader {
+        Tags {"Queue" = "Geometry-1" }
+        Lighting Off
+        Pass{
+            ZWrite On
+            ZTest LEqual
+            ColorMask 0
+        }
+    }
+}
--- Assets/Scripts/DugHole.cs
+++ Assets/Scripts/DugHole.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DugHole : MonoBehaviour
 {
+    public GameObject HoleMask;
     public const float DEPTH_PER_PASS = 1;
     public const float WIDTH_PER_DEPTH = 0.2f;
     public const float MAX_DEPTH = 10;
@@ -15,6 +16,7 @@ public class DugHole : MonoBehaviour
         _depth = Mathf.Clamp(_depth + DEPTH_PER_PASS,1, MAX_DEPTH);
 
         transform.localScale = new Vector3(1 + _depth * WIDTH_PER_DEPTH, _depth, transform.localScale.z);
+        HoleMask.transform.position = new Vector3(HoleMask.transform.position.x, 0, HoleMask.transform.position.z);
         //transform.Translate(0,-0.25f,0);
 
         if(_depth >= MAX_DEPTH)

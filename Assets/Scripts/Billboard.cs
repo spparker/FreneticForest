9a316842bae8ae70ab17b1834351c902a52d8a2f
spparker <sean.p.parker@gmail.com>
2023-02-05 02:01:48 -0800

    Multidirectional Movement and Patta v2

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -13,6 +13,12 @@ public class Billboard : MonoBehaviour {
     private Transform _mainCamTransform;
     private NavMeshAgent _agent;
     private SpriteRenderer _sr;
+
+    [SerializeField] private Sprite Forward;
+    [SerializeField] private Sprite ForwardSideways;
+    [SerializeField] private Sprite Sideways;
+    [SerializeField] private Sprite Backward;
+    [SerializeField] private Sprite BackwardSideways;
     private void Start()
     {
         _mainCamTransform = Camera.main.transform;
@@ -33,7 +39,11 @@ public class Billboard : MonoBehaviour {
             return;
         }
         
-        FlipToMoveDirection();
+        if(Forward == null)
+            FlipToMoveDirection();
+        else
+            FlipToMultidirection();
+
         AnimateWalk();
     }
 
@@ -49,6 +59,33 @@ public class Billboard : MonoBehaviour {
             _sr.flipX = false;
         else
             _sr.flipX = true;
+    }
+
+    private void FlipToMultidirection()
+    {
+        Vector3 flat_cam = Vector3.ProjectOnPlane(_mainCamTransform.forward, Vector3.up).normalized;
+        var angle = Vector3.SignedAngle(flat_cam, _agent.velocity.normalized, Vector3.up);
+        //Debug.Log("Angle: " + angle);
+        if(LeftFacing)
+            angle = -angle;
+
+        if(angle > 0)
+            _sr.flipX = false;
+        else
+            _sr.flipX = true;
+
+        var absAngle = Mathf.Abs(angle);
+
+        if(absAngle < 23)
+            _sr.sprite = Forward;
+        else if(absAngle < 68)
+            _sr.sprite = ForwardSideways;
+        else if(absAngle < 113)
+            _sr.sprite = Sideways;
+        else if(absAngle < 158)
+            _sr.sprite = BackwardSideways;
+        else
+            _sr.sprite = Backward;
     }
 
     private void AnimateWalk()
